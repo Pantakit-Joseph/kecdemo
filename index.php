4c7e21a12bdb91de0a9042bb9b6e640b86ee0cbc
Pantakit <70746412+Pantakit-Joseph@users.noreply.github.com>
2022-06-08 17:23:53 +0700

    setup0.3

--- index.php
+++ index.php
@@ -53,7 +53,13 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+	if (strstr($_SERVER['HTTP_HOST'], 'dev.')) {
+		define('ENVIRONMENT', 'development');
+	} else {
+		define('ENVIRONMENT', 'production');
+	}
 
 /*
  *---------------------------------------------------------------
