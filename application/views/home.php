75b39d5faa95b1f1fa5facef971022c94c01d522
Pantakit <70746412+Pantakit-Joseph@users.noreply.github.com>
2022-06-09 16:41:17 +0700

    v0.1

---
+++ application/views/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>วิทยาลัยการอาชีพแก้งคร้อ</title>
+	<link rel="stylesheet" href="<?= base_url('assets/css/custom.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/home.css') ?>">
+</head>
+
+<body>
+
+	<div class="home-container mx-auto my-5 text-center">
+
+		<img src="<?= base_url('assets/img/logo.png') ?>" alt="Logo" class="home-logo mx-auto d-block">
+		<h1 class="my-4">วิทยาลัยการอาชีพแก้งคร้อ</h1>
+
+		<div class="card">
+			<div class="card-body py-4">
+				<a href="#" class="btn btn-color1">หนังสือคำร้องขอฝึกอาชีพในสถานประกอบการ</a>
+			</div>
+		</div>
+	</div>
+
+	<script src="<?= base_url('assets/js/jquery-3.6.0.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/bootstrap.bundle.js') ?>"></script>
+</body>
+
+</html>
