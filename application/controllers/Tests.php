9a81ad0ed107d063fa0ee32be85cb93ba1d6b5a4
Pantakit <70746412+Pantakit-Joseph@users.noreply.github.com>
2022-06-10 15:50:44 +0700

    v0.4a

--- application/controllers/Tests.php
+++ application/controllers/Tests.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Test extends CI_Controller {
+class Tests extends CI_Controller {
 
 	public function Request_internship() {
 		$this->load->view('pn/request_internship_t');
